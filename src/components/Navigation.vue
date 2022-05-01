57a18d8b8dde96f379a801d8abd85020096a282c
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-05-01 00:46:38 -0400

    login state

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -9,13 +9,13 @@
         <router-link class="cursor-pointer" :to="{name: 'Home'}">
           Home
         </router-link>
-        <router-link class="cursor-pointer" :to="{name: ''}">
+        <router-link v-if="user" class="cursor-pointer" :to="{name: ''}">
           Create
         </router-link>
-        <router-link class="cursor-pointer" :to="{name: 'Login'}">
+        <router-link v-if="!user" class="cursor-pointer" :to="{name: 'Login'}">
           Login
         </router-link>
-        <li class="cursor-pointer">
+        <li v-if="user" @click="logout" class="cursor-pointer">
           Logout
         </li>
       </ul>
@@ -24,15 +24,26 @@
 </template>
 
 <script>
+import store from '../store/index';
+import { computed } from 'vue';
+import { supabase } from '../supabase/init';
+import { useRouter } from 'vue-router';
+
 export default {
   setup() {
     // Get user from store
+    const user = computed(() => store.state.user);
 
     // Setup ref to router
+    const router = useRouter();
 
     // Logout function
+    const logout = async () => {
+      await supabase.auth.signOut();
+      router.push({name: "Home"});
+    }
 
-    return {};
+    return {logout, user};
   },
 };
 </script>
