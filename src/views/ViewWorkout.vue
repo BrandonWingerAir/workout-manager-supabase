d067dbe91a4e43437aa8df655011ba2e0792eade
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-05-02 16:26:13 -0400

    route titles, authentication guarding and deploy settings

--- src/views/ViewWorkout.vue
+++ src/views/ViewWorkout.vue
@@ -227,7 +227,7 @@
         v-if="edit" 
         @click="update"
         type="button" 
-        class="mt-10 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
+        class="mt-10 py-2 px-6 rounded-sm text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
       >
         Update Workout
       </button>
